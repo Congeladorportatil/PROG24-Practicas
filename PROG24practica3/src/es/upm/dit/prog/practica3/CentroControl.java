f332277224ebb5f07480a8cfa55ca5e356e4eefa
Congeladorportatil <41164425+Congeladorportatil@users.noreply.github.com>
2025-01-05 17:38:38 +0100

    Add files via upload

---
+++ PROG24practica3/src/es/upm/dit/prog/practica3/CentroControl.java
@@ -0,0 +1,115 @@
+package es.upm.dit.prog.practica3;
+
+import java.util.Arrays;
+
+public class CentroControl {
+	private static final int N = 10;
+	private Dron[] drones;
+	private Mision[] misiones;
+
+	public CentroControl() {
+		this.drones = new Dron[N];
+		this.misiones = new Mision[N];
+	} 
+	
+	@Override
+	public String toString() {
+		return "CentroControl [drones=" + Arrays.toString(drones) + ", misiones=" + Arrays.toString(misiones) + "]";
+	}
+
+
+
+	public void addDron(Dron d) {
+		if (d!=null) {
+			int count = 0;
+			for (int i = 0; i<drones.length; i++) {
+				if (drones[i] == null) {
+					drones[i] = d;
+					count =1;
+					break;
+				}
+			}
+			if (count == 0) {
+				drones[0] = null;
+				for (int j = 1; j<drones.length; j++) {
+					drones[j-1] = drones[j];
+					drones[j] = null;
+				}
+				drones[drones.length -1] = d;
+			}
+		}
+	}
+	
+	public Dron[] getDrones() {
+		int cont = 0;
+		for (int i = 0; i < drones.length; i++) {
+			if (drones[i] != null)  {
+				cont++;
+			}
+		}
+		Dron[] lista = new Dron[cont];
+		cont = 0;
+		for (int i = 0; i < drones.length; i++) {
+			if (drones[i] != null)  {
+				lista[cont] = drones[i];
+				cont++;
+			}
+		}
+		return lista;
+	}
+	
+	public void addMision(Mision m) {
+		if (m!=null) {
+			int count = 0;
+			for (int i = 0; i<drones.length; i++) {
+				if (misiones[i] == null) {
+					misiones[i] = m;
+					count =1;
+					break;
+				}
+			}
+			if (count == 0) {
+				misiones[0] = null;
+				for (int j = 1; j<misiones.length; j++) {
+					misiones[j-1] = misiones[j];
+					drones[j] = null;
+				}
+				misiones[misiones.length -1] = m;
+			}
+		}
+	}
+	
+	public Mision[] getMisiones() {
+		int cont = 0;
+		for (int i = 0; i < misiones.length; i++) {
+			if (misiones[i] != null)  {
+				cont++;
+			}
+		}
+		Mision[] lista = new Mision[cont];
+		cont = 0;
+		for (int i = 0; i < drones.length; i++) {
+			if (misiones[i] != null)  {
+				lista[cont] = misiones[i];
+				cont++;
+			}
+		}
+		return lista;
+	}
+
+	public void update(long t) {
+		for (int i = 0; i< drones.length; i++) {
+			if (drones[i] != null) {
+				drones[i].mover(t);
+			}
+		}
+		
+		for (int i = 0; i< misiones.length; i++) {
+			if (misiones[i] != null) {
+				misiones[i].update(t);
+			}
+		}
+	}
+
+
+}
---
+++ PROG24practica3/src/es/upm/dit/prog/practica3/Mision.java
@@ -0,0 +1,101 @@
+	package es.upm.dit.prog.practica3;
+	
+	import java.util.Arrays;
+	import java.util.Objects;
+	
+	public class Mision {
+		private String id;
+		private Dron dron;
+		private Posicion[] posiciones;
+		private long[] tiempos;
+		private int nPosiciones;
+		private int posicion;
+	
+		public Mision(String id, Dron dron, int maxPosiciones) {
+			this.id = id;
+			this.dron = dron;
+			this.posiciones = new Posicion[maxPosiciones];
+			this.tiempos = new long[maxPosiciones];
+			this.nPosiciones = 0;
+			this.posicion = 0;
+		}
+	
+		public String getId() {
+			return this.id;
+		}
+	
+		public Dron getDron() {
+			return this.dron;
+		}
+	
+		public Posicion[] getPosiciones() {
+			return this.posiciones;
+		}
+	
+		public long[] getTiempos() {
+			return this.tiempos;
+		}
+	
+		public int getnPosiciones() {
+			return this.nPosiciones;
+		}
+	
+		public int getPosicion() {
+			return this.posicion;
+		}
+	
+		@Override
+		public int hashCode() {
+			return Objects.hash(id);
+		}
+	
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Mision other = (Mision) obj;
+			return Objects.equals(id, other.id);
+		}
+	
+		@Override
+		public String toString() {
+			return "Mision [id=" + id + ", dron=" + dron + ", posiciones=" + Arrays.toString(posiciones) + ", tiempos="
+					+ Arrays.toString(tiempos) + ", nPosiciones=" + nPosiciones + ", posicion=" + posicion + "]";
+		}
+	
+		
+		public void addPosT(Posicion pos, long t) {
+			if (pos != null && this.nPosiciones < this.posiciones.length) {
+				this.posiciones[this.nPosiciones] = pos;
+				this.tiempos[this.nPosiciones] = t;
+				this.nPosiciones++;
+			}
+		}
+	
+		public boolean activa() {
+			return this.posicion < this.nPosiciones && this.posiciones[this.posicion] != null;
+		}
+	
+		public void update(long t) {
+			if (activa() && dron.getT() == t && tiempos[posicion] == t ) {
+				Posicion vel = new  Posicion(0,0,0);
+				if (posicion < nPosiciones -1) {
+					long diff = tiempos[posicion+1] - tiempos[posicion];
+					if (t>=0) {
+						double velx = (posiciones[posicion+1].getX() - posiciones[posicion].getX())/diff;
+						double vely = (posiciones[posicion+1].getY() - posiciones[posicion].getY())/diff;
+						double velz = (posiciones[posicion+1].getZ() - posiciones[posicion].getZ())/diff;
+						vel = new Posicion(velx,vely,velz);
+					} 
+				}
+				dron.setVel(vel);
+				posicion++;
+			}
+		}
+	}
+	
+	
